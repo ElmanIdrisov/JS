5a0fcf77bc69c357b0d0237c69002167c73f5c43
ElmanIdrisov <elmanidrisov222@gmail.com>
2022-12-08 14:28:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/* const age = '18';
+let name = 'Elman'; */
+
+const age = 18; // * number
+const name = 'Elman'; // * string
+const isHungry = true; // * boolean - true/false
+const nothing = null; // * null - nothing
+const arr = [1, 2, 3 ,4]; // * array on numbers
+
+const person = {
+    name: 'Elman',
+    age: 18,
+    cityName: 'Almaty'
+} // * object
+
+
+
+console.log(name);
+
