529ae2bd1b9a92aa510607d7f1254b6eec46c3f6
ElmanIdrisov <elmanidrisov222@gmail.com>
2022-12-09 18:32:40 +0600

    add function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* const age = '18';
 let name = 'Elman'; */
 
-const age = 18; // * number
-const name = 'Elman'; // * string
+/* const age = 18; // * number
+const myName = 'Elman'; // * string
 const isHungry = true; // * boolean - true/false
 const nothing = null; // * null - nothing
 const arr = [1, 2, 3 ,4]; // * array on numbers
@@ -13,7 +13,110 @@ const person = {
     cityName: 'Almaty'
 } // * object
 
+console.log(myName); */
+
+/* const person = {
+    myName: 'Elman',
+    age: 18,
+    adress: 'Kaskelen',
+    familyQuantity: 1,
+    favoriteFood: ['pizza', 'apple']
+}
+
+console.log(person.favoriteFood)
+ */
+
+// // * математические операторы
+// console.log(x + y); // * plus
+// console.log(x / y); // * divaid
+// console.log(x * y); // * multiply
+// console.log(x - y); // * minus
+
+// const some = 1 + 1 + 3 + '2';
+// console.log(some);
+
+// * || или
+// * && и
+
+// * > больше
+// * < меньше
+
+// * == не строгое сравнение
+// * === строгое сравнение
+// * != не равно
 
 
-console.log(name);
 
+/* 
+const personOne = {
+    name: 'Max',
+    age: 21
+}
+
+const personTwo = {
+    name: 'Jack',
+    age: 22
+}
+
+if( personOne.age == personTwo.age){
+    console.log('Они ровестники')
+} else {
+    console.log('Они не ровестники');
+    console.log(personOne.name + ' ' + personOne.age);
+    console.log(personTwo.name + ' ' + personTwo.age);
+}
+
+const fruit = 'apple';
+const fruitTwo = 'apple';
+
+
+if (fruit === fruitTwo) {
+    console.log('Равен');
+}
+ */
+/* 
+const button = document.querySelector('button');
+
+function changeButtonColor() {
+button.style.backgroundColor = 'yellow';
+}
+
+button.addEventListener ('click', function() {
+    changeButtonColor();
+}) */
+
+/* const button = document.querySelector('button');
+ function changeButtonWidth() {
+    button.style.width = '100px'
+ }
+
+ button.addEventListener ('click', function(){
+    changeButtonWidth();
+ }) */
+
+/*  const arr = ['apple', 'banan', 'pineapple'];  
+
+ /* const banan = arr[1];
+ 
+ console.log('banana');
+ */
+
+/* const arrLength = arr.length;
+console.log(arr.Length);
+
+
+
+if (arr.length >= 3){
+    console.log('ok')
+} else{
+    console.log('Меньше 3-х');
+} */ 
+
+
+const arr = ['apple', 'banan', 'pineapple']; 
+
+if (arr.Length) {
+    console.log('Не пусто');
+} else {
+    console.log('Пусто');
+}
