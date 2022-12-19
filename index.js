fc66baeadba21619cdb78df9e8a6063776d5a21b
ElmanIdrisov <elmanidrisov222@gmail.com>
2022-12-19 21:54:59 +0600

    first commit

--- index.js
+++ index.js
@@ -1,145 +1,49 @@
-/* const age = '18';
+/* const age = 18;
 let name = 'Elman'; */
 
-/* const age = 18; // * number
-const myName = 'Elman'; // * string
-const isHungry = true; // * boolean - true/false
+/* const age = 22; // * number
+const name = 'Elman'; // * string
+const isMale = true; // * boolean - true/false
 const nothing = null; // * null - nothing
-const arr = [1, 2, 3 ,4]; // * array on numbers
+const notFound = undefined; // * not found
+const arr = [1, 2, 3, 4]; // * array of numbers
 
-const person = {
+const obj = {
     name: 'Elman',
-    age: 18,
+    age: 22,
     cityName: 'Almaty'
 } // * object
 
-console.log(myName); */
+console.log(age );
 
-/* const person = {
-    myName: 'Elman',
+const boxElement = document.querySelector('.box');
+boxElement.style.backgroundColor = 'yellow';
+console.log(boxElement); */
+
+/*  const person = {
+    name: 'Elman',
     age: 18,
-    adress: 'Kaskelen',
-    familyQuantity: 1,
-    favoriteFood: ['pizza', 'apple']
+    adress: 'Almaty',
+    familyQuantity: 4,
+    favoriteFood: ['apple', 'pizza', 'banana', 'potato']
 }
 
-console.log(person.favoriteFood)
+console.log(person.favoriteFood);  */
+
+/* const arr = ['cat', 'dog', 'bird', 'mouse', 'human'];
+
+const mouse = arr[3];
+
+console.log(mouse); */
+
+/* function showInConsole(message, number, adress){
+    console.log(message, number, adress);
+}
+
+showInConsole('Elman', 22, 'Almaty')
  */
 
-// // * математические операторы
-// console.log(x + y); // * plus
-// console.log(x / y); // * divaid
-// console.log(x * y); // * multiply
-// console.log(x - y); // * minus
-
-// const some = 1 + 1 + 3 + '2';
-// console.log(some);
-
-// * || или
-// * && и
-
-// * > больше
-// * < меньше
-
-// * == не строгое сравнение
-// * === строгое сравнение
-// * != не равно
-
-
-
-/* 
-const personOne = {
-    name: 'Max',
-    age: 21
-}
-
-const personTwo = {
-    name: 'Jack',
-    age: 22
-}
-
-if( personOne.age == personTwo.age){
-    console.log('Они ровестники')
-} else {
-    console.log('Они не ровестники');
-    console.log(personOne.name + ' ' + personOne.age);
-    console.log(personTwo.name + ' ' + personTwo.age);
-}
-
-const fruit = 'apple';
-const fruitTwo = 'apple';
-
-
-if (fruit === fruitTwo) {
-    console.log('Равен');
-}
- */
-/* 
-const button = document.querySelector('button');
-
-function changeButtonColor() {
-button.style.backgroundColor = 'yellow';
-}
-
-button.addEventListener ('click', function() {
-    changeButtonColor();
-}) */
-
-/* const button = document.querySelector('button');
- function changeButtonWidth() {
-    button.style.width = '100px'
- }
-
- button.addEventListener ('click', function(){
-    changeButtonWidth();
- }) */
-
-/*  const arr = ['apple', 'banan', 'pineapple'];  
-
- /* const banan = arr[1];
- 
- console.log('banana');
- */
-
-/* const arrLength = arr.length;
-console.log(arr.Length);
-
-
-
-if (arr.length >= 3){
-    console.log('ok')
-} else{
-    console.log('Меньше 3-х');
-} */ 
-
-/* 
-const arr = ['apple', 'banan', 'pineapple']; 
-
-if (arr.Length) {
-    console.log('Не пусто');
-} else {
-    console.log('Пусто');
-} */
-
-/* 
-const arr = ['milk', 'egg', 'apple', 'oil', 'potato', 'banana', 'watermelon']
-let selectedProduct = null;
-
-for (let index = 0; index < arr.length; index++) {
-    const product = arr[index];
-    
-    if (product === 'apple'){
-        selectedProduct = apple;
-    }
-}
-
-console.log('selectedProduct', selectedProduct); */
-
-
-/* const arrOfNum = [10, 20, 30, 40, 50];
-
-const changedArr = arrOfNum.map ( (number) => {
-    return number * 2;
-});
-
-console.log('changedArr', changedArr); */
+/* console.log(0.2 + 0.3); // * plus
+console.log(2 - 1); // * minus
+console.log(2 * 5); // * multiply
+console.log(6 / 3); // * divaid */
