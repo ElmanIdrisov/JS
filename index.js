adf868456c25854e82203c29195c4618e50a9595
ElmanIdrisov <elmanidrisov222@gmail.com>
2022-12-20 14:13:54 +0600

    second commit

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ const mouse = arr[3];
 
 console.log(mouse); */
 
+
+// * чуть чуть про функции
 /* function showInConsole(message, number, adress){
     console.log(message, number, adress);
 }
@@ -47,3 +49,135 @@ showInConsole('Elman', 22, 'Almaty')
 console.log(2 - 1); // * minus
 console.log(2 * 5); // * multiply
 console.log(6 / 3); // * divaid */
+
+// const some = 1 + 1 + 1 + '2'; // * 32
+
+// * || или
+// * && и
+
+// * == не строгое сравнение
+// * === строгое сравнение
+// * != не равно
+
+// сonst name = 'Elman';
+
+// if (name == 'Elman'){
+    //console.log('name is Elman')
+// } else {
+    // console.log('not Elman')
+// }
+
+/* const banana = 'banana';
+const apple = 'banana';
+
+const x = 5;
+
+if (banana == apple) {
+    console.log('is banan');
+} else {
+    console.log('not banan');
+} */
+
+/* const x = 5;
+const y = 4;
+
+if (x == y) {
+    console.log('x == y');
+} else {
+    console.log('x != y');
+} */
+
+/* const personOne = {
+    name: 'Max',
+    age: 21
+}
+
+const personTwo = {
+    name: 'Jack',
+    age: 22
+}
+
+if (personOne.age == personTwo.age) {
+    console.log('Они ровесники');
+} else {
+    console.log('Они не ровестники');
+    console.log(personOne.name + ': ' + personOne.age);
+    console.log(personTwo.name + ': ' + personTwo.age);
+} */
+
+/* const fruit = 'apple';
+const fruitTwo = 'apple';
+
+const numOne = 1; // * number
+const numTwo = '1' // * string
+
+if (numOne === numTwo) {
+    console.log('Равен');
+} */
+
+/* const button = document.querySelector('button');
+
+function changeButtonColor() {
+    button.style.width = '150px';
+}
+
+button.addEventListener('click', function() {
+    changeButtonColor();
+} ) */
+
+ /* const arr = ['banana', 'apple', 'pineapple']; */
+
+/* if (arr.length >= 3) {
+    alert('Ok')
+} else {
+    alert ('Меньше 3')
+}  */
+
+/* if (arr.length) {
+    console.log('Не пусто');
+} else {
+    console.log('Пусто');
+} */
+
+/* const x = 10;
+const y = 8;
+const t = 12;
+const v = 14;
+
+console.log(x + t);
+console.log(v - y);
+console.log(t * v);
+console.log(x / y); */
+
+/* const personOne = {
+    name: 'Messi',
+    age: 35,
+    hobbie: 'football'
+}
+
+const personTwo = {
+    name: 'Ronaldo',
+    age: 37,
+    hobbie: 'tennis'
+}
+
+if (personOne.hobbie === personTwo.hobbie) {
+    console.log('Хобби совпадают');
+} else {
+    console.log('Хобби не совпадают');
+    console.log('personOne' + ': ', 'football');
+    console.log('personTwo' + ': ', 'tennis');
+} */
+
+/* const x = 1;
+const y = 1;
+let result = 0;
+
+function plus (name){
+    console.log('my name is ', name);
+}
+
+plus('Elman');
+plus('Erlan');
+plus('Dauren')
+ */
